b76289ee37272fa2527880e2ff01af29c15952c4
Nelson Orellana <55712980+Nels2@users.noreply.github.com>
2022-01-17 10:58:51 -0600

    Update srt2.ps1
    change a couple of thigns with the way directories are removed as well as adding additional services I missed.

--- srt2.ps1
+++ srt2.ps1
@@ -22,7 +22,7 @@ Function createAdminRights{
     $rule = New-Object System.Security.AccessControl.FileSystemAccessRule("Domain Admins", "FullControl", "ContainerInherit,ObjectInherit", "None", "Allow")
     write-host ("*** creating admin rights rules for this script to work properly...")
     foreach ($DirFile in $acl_directories) {
-        takeown /f "$DirFile" /r /a
+        takeown /f "$DirFile" /r /a /D y
         $acl = get-acl $DirFile
         $acl.AddAccessRule($rule)
         Set-Acl $DirFile $acl
@@ -32,7 +32,7 @@ Function createAdminRights{
 
 #Stop All Sophos Services
 Function stop_DemSophosServices{
-    $services = 'Sophos AutoUpdate Service', 'Sophos Agent', 'SAVService', 'SAVAdminService', 'Sophos Message Router', 'Sophos Web Control Service', 'swi_service', 'swi_update', 'SntpService', 'Sophos System Protection Service', 'Sophos Web Control Service', 'Sophos Endpoint Defense Service', 'HitmanPro.Alert service', 'Sophos Clean Service', 'Sophos MCS Agent', 'Sophos MCS Client' , 'Sophos Device Encryption Service', 'Sophos Health Service', 'Sophos Safestore Service', 'Sophos File Scanner Service' #turned into an array as the list was just too long, so why not make it a function.
+    $services = 'Sophos AutoUpdate Service', 'hmpalertsvc', 'Sophos Agent', 'SAVService', 'SAVAdminService', 'Sophos Message Router', 'Sophos Web Control Service', 'swi_service', 'swi_update', 'SntpService', 'Sophos System Protection Service', 'Sophos Web Control Service', 'Sophos Endpoint Defense Service', 'HitmanPro.Alert service', 'Sophos Clean Service', 'Sophos MCS Agent', 'Sophos MCS Client' , 'Sophos Device Encryption Service', 'Sophos Health Service', 'Sophos Safestore Service', 'Sophos File Scanner Service', 'Sophos Device Control Service' #turned into an array as the list was just too long, so why not make it a function.
 
     write-host ("*** running loop to stop all Sophos services...")
     foreach ($element in $services) {
@@ -42,7 +42,7 @@ Function stop_DemSophosServices{
 
 #Kill all Sophos Services
 function sophosServicesAnFonem_taskkill{
-    $tasks2kill = 'ALMon.exe', 'ALsvc.exe', 'swi_fc.exe', 'swi_filter.exe', 'spa.exe', 'Sophos UI.exe', 'SophosDiag.exe', 'SavApi.exe'
+    $tasks2kill = 'ALMon.exe', 'ALsvc.exe', 'swi_fc.exe', 'swi_filter.exe', 'spa.exe', 'Sophos UI.exe', 'SophosDiag.exe', 'SavApi.exe', 'SEDService.exe', 'hmpalert.exe', 'EXPTelem.exe'
 
     write-host ("*** force killing any Sophos Services..")
     foreach ($item in $tasks2kill) {
@@ -52,7 +52,7 @@ function sophosServicesAnFonem_taskkill{
 
 #uninstall everything with sophos
 function sophosRegistry_Removal {
-    $sName = 'Sophos Network Threat Protection', 'Sophos System Protection', 'Sophos Client Firewall', 'Sophos Anti-Virus', 'Sophos Remote Management System', 'Sophos AutoUpdate', 'Sophos EndpointDefense', 'Sophos Endpoint Firewall', 'Sophos Endpoint Self Help', 'Sophos Heartbeat', 'Sophos Management', 'Sophos AMSI Protection', 'Sophos File Scanner', 'Sophos ML Engine', 'Sophos Standalone Engine', 'Sophos Endpoint Agent', 'Sophos Exploit Protection', 'Sophos Clean'
+    $sName = 'Sophos Network Threat Protection', 'Sophos System Protection', 'Sophos Client Firewall', 'Sophos Anti-Virus', 'Sophos Remote Management System', 'Sophos AutoUpdate', 'Sophos Endpoint Defense', 'Sophos Endpoint Firewall', 'Sophos Endpoint Self Help', 'Sophos Heartbeat', 'Sophos Management', 'Sophos AMSI Protection', 'Sophos File Scanner', 'Sophos ML Engine', 'Sophos Standalone Engine', 'Sophos Endpoint Agent', 'Sophos Exploit Protection', 'Sophos Clean', 'Sophos Management Communications System', 'Sophos Health', 'Sophos Diagnostic Utility', 'Sophos Data Protection Agent 2.1.182.0'
     write-host ("*** unininstalling Sophos and its related software..")
     foreach ($regItemd in $sName)  {
         $StartVer = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall  |
@@ -76,10 +76,11 @@ function sophosRegistry_Removal {
 
 #Directory Cleanup --added sub directories as windows has issues removing them even with -recursive.
 function sophosDirectory_Removal {
-    $pathsTodelete = 'C:\Program Files\Sophos\Endpoint Self Help', 'C:\Program Files\Sophos\Sophos AMSI Protection', 'C:\Program Files\Sophos\SophosUI', 'C:\Program Files\Sophos', 'C:\Program Files (x86)\Sophos\Sophos AMSI Protection', 'C:\Program Files (x86)\Sophos\Sophos Data Protection', 'C:\Program Files (x86)\Sophos', 'C:\ProgramData\HitanPro.Alert', 'C:\ProgramData\Sophos\Management Communications System', 'C:\ProgramData\Sophos\Remote Management System', 'C:\ProgramData\Sophos\Sophos AMSI Protection', 'C:\ProgramData\Sophos\Sophos Anti-Virus', 'C:\ProgramData\Sophos\Sophos Data Protection', 'C:\ProgramData\Sophos\Sophos Diagnostic Utility', 'C:\ProgramData\Sophos\Sophos File Scanner', 'C:\ProgramData\Sophos\Sophos UI', 'C:\ProgramData\Sophos\Web Intelligence', 'C:\ProgramData\Sophos', 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Sophos'
+    $pathsTodelete = 'C:\Program Files\Sophos\Endpoint Self Help', 'C:\Program Files\Sophos\Sophos AMSI Protection', 'C:\Program Files\Sophos\SophosUI', 'C:\Program Files\Sophos', 'C:\Program Files (x86)\Sophos\Sophos AMSI Protection', 'C:\Program Files (x86)\Sophos\Sophos Data Protection', 'C:\Program Files (x86)\Sophos', 'C:\ProgramData\HitanPro.Alert', 'C:\ProgramData\Sophos\Management Communications System', 'C:\ProgramData\Sophos\Remote Management System', 'C:\ProgramData\Sophos\Sophos AMSI Protection', 'C:\ProgramData\Sophos\Sophos Anti-Virus', 'C:\ProgramData\Sophos\Sophos Data Protection', 'C:\ProgramData\Sophos\Sophos Diagnostic Utility', 'C:\ProgramData\Sophos\Sophos File Scanner', 'C:\ProgramData\Sophos\Sophos UI', 'C:\ProgramData\Sophos\Web Intelligence', 'C:\ProgramData\Sophos', 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Sophos', 'C:\ProgramData\Sophos\Safestore', 'C:\ProgramData\Sophos\Sophos Tamper Protection', 'C:\ProgramData\Sophos\Web Intelligence'
     write-host ("*** cleaning directories related to Sophos...")
     foreach ($dirItem in $pathsTodelete) {
-        Get-ChildItem "$dirItem" -Include * -Recurse | Remove-Item -Force -Recurse -Confirm:$false
+        #Get-ChildItem "$dirItem" -Include * -Recurse
+        Remove-Item "$dirItem" -Recurse
     }
 }
 
@@ -97,6 +98,7 @@ wmic service where "caption like '%Sophos%'" call stopservice #Redundant "Stop S
 sophosServicesAnFonem_taskkill
 sophosRegistry_Removal
 createAdminRights
+stop_DemSophosServices # rerun to make sure services are stopped before removing directories, as they will not remove if they are being used.
 sophosDirectory_Removal
 #Remove Registry Keys
 write-host ("*** deleting misc. sophos registry values...")
